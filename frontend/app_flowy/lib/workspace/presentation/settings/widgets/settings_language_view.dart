fb6d1e8e3a56f85b53469ae6c10bd70d1eda617d
sunny7dusk <nat233947@gmail.com>
2022-07-29 09:29:54 -0400

    refactor: WIP overlay is behing settings dialog

--- frontend/app_flowy/lib/workspace/presentation/settings/widgets/settings_language_view.dart
+++ frontend/app_flowy/lib/workspace/presentation/settings/widgets/settings_language_view.dart
@@ -1,9 +1,8 @@
+import 'package:app_flowy/workspace/presentation/widgets/locale_picker/locale_picker.dart';
 import 'package:app_flowy/generated/locale_keys.g.dart';
 import 'package:app_flowy/workspace/application/appearance.dart';
 import 'package:easy_localization/easy_localization.dart';
-import 'package:flowy_infra/theme.dart';
 import 'package:flutter/material.dart';
-import 'package:flowy_infra/language.dart';
 import 'package:provider/provider.dart';
 
 class SettingsLanguageView extends StatelessWidget {
@@ -11,7 +10,6 @@ class SettingsLanguageView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    context.watch<AppTheme>();
     return ChangeNotifierProvider.value(
       value: Provider.of<AppearanceSettingModel>(context, listen: true),
       child: SingleChildScrollView(
@@ -47,66 +45,14 @@ class LanguageSelectorDropdown extends StatefulWidget {
 }
 
 class _LanguageSelectorDropdownState extends State<LanguageSelectorDropdown> {
-  Color currHoverColor = Colors.white.withOpacity(0.0);
-  late Color themedHoverColor;
-  void hoverExitLanguage() {
-    setState(() {
-      currHoverColor = Colors.white.withOpacity(0.0);
-    });
-  }
-
-  void hoverEnterLanguage() {
-    setState(() {
-      currHoverColor = themedHoverColor;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    final theme = context.watch<AppTheme>();
-    themedHoverColor = theme.main2;
-
-    return MouseRegion(
-      onEnter: (event) => {hoverEnterLanguage()},
-      onExit: (event) => {hoverExitLanguage()},
-      child: Container(
-        margin: const EdgeInsets.only(left: 8, right: 8),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(8),
-          color: currHoverColor,
-        ),
-        child: DropdownButtonHideUnderline(
-          child: DropdownButton<Locale>(
-            value: context.read<AppearanceSettingModel>().locale,
-            onChanged: (val) {
-              setState(() {
-                context.read<AppearanceSettingModel>().setLocale(context, val!);
-              });
-            },
-            icon: const Visibility(
-              child: (Icon(Icons.arrow_downward)),
-              visible: false,
-            ),
-            borderRadius: BorderRadius.circular(8),
-            items: EasyLocalization.of(context)!.supportedLocales.map((locale) {
-              return DropdownMenuItem<Locale>(
-                value: locale,
-                child: Padding(
-                  padding: const EdgeInsets.all(12.0),
-                  child: Text(
-                    languageFromLocale(locale),
-                    style: TextStyle(
-                      fontSize: 14,
-                      fontWeight: FontWeight.w500,
-                      color: theme.textColor,
-                    ),
-                  ),
-                ),
-              );
-            }).toList(),
-          ),
-        ),
+    return Container(
+      margin: const EdgeInsets.only(left: 8, right: 8),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(8),
       ),
+      child: const LocalePicker(),
     );
   }
 }
---
+++ frontend/app_flowy/lib/workspace/presentation/widgets/locale_picker/locale_picker.dart
@@ -0,0 +1,71 @@
+import 'package:app_flowy/startup/tasks/app_widget.dart';
+import 'package:app_flowy/workspace/application/appearance.dart';
+import 'package:dartz/dartz.dart' as dartz;
+import 'package:app_flowy/workspace/presentation/widgets/pop_up_action.dart';
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flowy_infra/language.dart';
+import 'package:flowy_infra/theme.dart';
+import 'package:flowy_infra_ui/flowy_infra_ui_web.dart';
+import 'package:flowy_infra_ui/style_widget/button.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class LocaleActionItem extends ActionItem {
+  Locale given;
+
+  LocaleActionItem(this.given);
+  @override
+  Widget? get icon => null;
+
+  @override
+  String get name => languageFromLocale(given);
+
+  Locale get locale => given;
+}
+
+class LocalePickerActionSheet extends ActionList<LocaleActionItem> with FlowyOverlayDelegate {
+  late final List<Locale> _items;
+  final Function(dartz.Option<LocaleActionItem>) onSelected;
+  final BuildContext context;
+
+  LocalePickerActionSheet({Key? key, required this.onSelected, required this.context});
+
+  @override
+  FlowyOverlayDelegate? get delegate => this;
+
+  @override
+  List<LocaleActionItem> get items => _items.map((curr) => LocaleActionItem(curr)).toList();
+
+  @override
+  void Function(dartz.Option<LocaleActionItem> p1) get selectCallback => (result) {
+        result.fold(() => onSelected(dartz.none()), (a) => onSelected(dartz.some(a)));
+      };
+
+  LocalePickerActionSheet buildLocaleList() {
+    _items = EasyLocalization.of(context)!.supportedLocales.map((e) => e).toList();
+    return this;
+  }
+}
+
+class LocalePicker extends StatelessWidget {
+  const LocalePicker({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Builder(builder: (dialogContext) {
+      return TextButton(
+        onPressed: () {
+          final actionSheet = LocalePickerActionSheet(
+                  onSelected: (given) {
+                    final getLocale = given.fold(() => null, (a) => a);
+                    context.read<AppearanceSettingModel>().setLocale(context, getLocale!.given);
+                  },
+                  context: context)
+              .buildLocaleList();
+          actionSheet.show(context, anchorContext: dialogContext);
+        },
+        child: Text(languageFromLocale(context.read<AppearanceSettingModel>().locale)),
+      );
+    });
+  }
+}
